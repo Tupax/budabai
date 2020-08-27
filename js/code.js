931480847212f82417e5d563cf15158f68c2e6de
Small Potion <hello@smallpotion.co>
2020-08-27 14:04:11 -0300

    limpieza

--- js/code.js
+++ js/code.js
@@ -1,5 +1,6 @@
 $("document").ready(function () {
   GetMes();
+  GetFecha();
 });
 
 /* Funcion para indicar el mes */
@@ -18,15 +19,68 @@ function GetMes() {
     "Noviembre",
     "Diciembre",
   ];
-  var today = new Date();
   var aMonth = today.getMonth();
   console.log(theMonths[aMonth]);
   $("#mes").text(theMonths[aMonth]);
   //  document.write(theMonths[aMonth]);
 }
 
+const today = new Date();
+
+const theDays = [
+  "Domingo",
+  "Lunes",
+  "Martes",
+  "Miercoles",
+  "Jueves",
+  "Viernes",
+  "Sabado",
+  "Domingo",
+];
+
+/* Funcion para poner las fechas a los respectivos dias de las actividades */
 function GetFecha() {
   var today = new Date();
+  var todayWord = theDays[today.getDay()];
+  console.log(typeof theDays);
+  // console.log(todayWord);
+  /* 
+Obtener la lista con todos los dias y actividades
+
+Agarro la label .activity-checkbox dentro:
+div .checkbox-text
+input .w-checkbox-input.checkbox
+*/
+
+  var diasText = $(".checkbox-text");
+  console.log(diasText);
+  console.log(diasText.length);
+  console.log(typeof diasText);
+
+  for (let i = 0; i < diasText.length; i++) {
+    let dia = diasText[i].innerHTML.split(" ")[0];
+    let iday = theDays.indexOf(dia);
+
+    if (diasText[i].children[0] != undefined) {
+      let proxFechaObj = getNextDayOfWeek(today, iday);
+      diasText[i].children[0].innerText = proxFechaObj.getDate().toString();
+    }
+  }
+
+  function getNextDayOfWeek(date, dayOfWeek) {
+    // Code to check that date and dayOfWeek are valid left as an exercise ;)
+
+    var resultDate = new Date(date.getTime());
+
+    resultDate.setDate(date.getDate() + ((7 + dayOfWeek - date.getDay()) % 7));
+
+    return resultDate;
+  }
+
+  // diasText.forEach((diaText) => {
+  //   console.log(diaText.innerText());
+  //   console.log(diaText.text());
+  // });
 }
 
 /* Funcionalidad para mostrar mostrar el deatalle de la actividad en el form, eso es algo unicamente visual
@@ -37,7 +91,7 @@ $(".checkbox").click(function () {
   // console.log(check);
   var actividad = check.attr("add-act");
   var detalle = actividad.split("_");
-  // console.log(actividad);
+  console.log(actividad);
   // console.log(detalle);
   // console.log("el detalle en 0 : ", detalle[0]);
   // console.log(($('#' +detalle[0])).length);
@@ -62,10 +116,11 @@ $(".checkbox").click(function () {
              
              */
     if ($("#" + detalle[0]).length === 0) {
-      //create new li
-      var clon = $("#detail_list li:first").clone().attr("id", detalle[0]);
-      console.log(clon.children.length);
-      $("#detail_list").append(clon);
+      //create new element on summary
+      // OLD var clon = $("#detail_list li:first").clone().attr("id", detalle[0]);
+      var clon = $("#template").clone().attr("id", detalle[0]);
+      // console.log(clon.children.length);
+      $("#detalle").append(clon);
       $("#" + detalle[0]).show("slow");
       $("#" + detalle[0])
         .find("p.activity-item")
@@ -95,23 +150,23 @@ $(".checkbox").click(function () {
         .find("p.day-item")
         .text()
         .split(" - ");
-      console.log("antes de cortar ", dias);
+      // console.log("antes de cortar ", dias);
       for (let index = 0; index < dias.length; index++) {
         if (dias[index] === detalle[3]) {
           dias.splice(index, 1);
         }
       }
-      console.log("desp de cortar ", dias);
+      // console.log("desp de cortar ", dias);
 
-      console.log(dias);
+      // console.log(dias);
       $("#" + detalle[0])
         .find("p.day-item")
         .text(dias);
       dias = [];
 
-      console.log(
-        "el item con la actividad ya existe, habria que sacar el dia indicado por el checkbox, pero no eliminar"
-      );
+      // console.log(
+      //   "el item con la actividad ya existe, habria que sacar el dia indicado por el checkbox, pero no eliminar"
+      // );
     } else {
       console.log("eliminar");
       $("#" + detalle[0]).hide("slow");
@@ -120,31 +175,27 @@ $(".checkbox").click(function () {
         .delay(2000);
     }
 
-    console.log(dia_alternativo);
+    // console.log(dia_alternativo);
   }
 });
 
 $("#mision").click(function () {
   $("#tab-mision").click();
-  console.log("click");
   topFunction();
   return false;
 });
 $("#actividades").click(function () {
   $("#tab-actividades").click();
-  console.log("click");
   topFunction();
   return false;
 });
 $("#espacio").click(function () {
   $("#tab-espacio").click();
-  console.log("click");
   topFunction();
   return false;
 });
 $("#contacto").click(function () {
   $("#tab-contacto").click();
-  console.log("click");
   topFunction();
   return false;
 });
